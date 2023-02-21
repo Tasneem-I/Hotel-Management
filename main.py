a115c89cffc5b8d259165240374336b3bbc54c4e
Tasneem-I <107906533+Tasneem-I@users.noreply.github.com>
2023-02-21 18:34:15 +0530

    Changed to recent flask setup

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-from hotel import create_app
+from flask import Flask, render_template
 
-app = create_app()
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run('0.0.0.0', debug=True)
