679953c481819918441fc50aa835628628069aaf
Tasneem-I <107906533+Tasneem-I@users.noreply.github.com>
2023-03-04 19:44:28 +0530

    initializing database

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hotel.db'
+db = SQLAlchemy(app)
 
 @app.route('/')
 def home():
@@ -9,6 +12,12 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+
+class Reservation(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', debug=True)
