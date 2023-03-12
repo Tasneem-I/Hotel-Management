ef85146955601dbce67abb0ff8eb0057886ce9c3
sahettya <126938987+sahettya@users.noreply.github.com>
2023-03-12 12:03:18 +0530

    updated

--- main.py
+++ main.py
@@ -5,6 +5,17 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hotel.db'
 db = SQLAlchemy(app)
 
+class Room_Booking(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    room_number = db.Column(db.Integer, nullable=False)
+    customer_name = db.Column(db.String(30), nullable=False)
+    contact_no = db.Column(db.Integer, nullable=False)
+    email = db.Column(db.String(50), nullable=False)
+    guests = db.Column(db.Integer, nullable=False)
+    check_in = db.Column(db.date, nullable=False)
+    check_out = db.Column(db.date, nullable=False)
+    room_type = db.Column(db.String(30), nullable=False)
+
 @app.route('/')
 def home():
     return render_template('home.html')
@@ -13,15 +24,27 @@ def home():
 def about():
     return render_template('about.html')
 
-@app.route('/reserve')
+@app.route('/reserve',methods=['GET', 'POST'])
 def room():
+    if request.method == 'POST':
+        customer_name = request.form[]
+        check_in = request.form['check_in']
+        check_out = request.form['check_out']
+        guests = request.form['guests']
+        
+
+    
+    
+    
+    
+    
+    
+    
+    
     return render_template('book_room.html')
-class Room_Booking(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    room_number = db.Column(db.Integer, nullable=False)
-    customer_name = db.Column(db.String(30), nullable=False)
-    contact_no = db.Column(db.Integer, nullable=False)
-    email = db.Column(db.String(50), nullable=False)
+
+
+
 
     
 
